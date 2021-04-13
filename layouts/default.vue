6cecc5295d4ff2023535a739777e653986feba1b
andochiwa <a1066079469@gmail.com>
2021-04-13 20:21:09 +0900

    router and static page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,139 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="in-wrap">
+    <!-- 公共头引入 -->
+    <header id="header">
+      <section class="container">
+        <h1 id="logo">
+          <a href="#" title="谷粒学院">
+            <img src="@/assets/img/logo.png" width="100%" alt="谷粒学院">
+          </a>
+        </h1>
+        <div class="h-r-nsl">
+          <ul class="nav">
+            <router-link to="/" tag="li" active-class="current" exact>
+              <a>首页</a>
+            </router-link>
+            <router-link to="/course" tag="li" active-class="current">
+              <a>课程</a>
+            </router-link>
+            <router-link to="/teacher" tag="li" active-class="current">
+              <a>名师</a>
+            </router-link>
+            <router-link to="/article" tag="li" active-class="current">
+              <a>文章</a>
+            </router-link>
+            <router-link to="/qa" tag="li" active-class="current">
+              <a>问答</a>
+            </router-link>
+          </ul>
+          <!-- / nav -->
+          <ul class="h-r-login">
+            <li id="no-login">
+              <a href="/sing_in" title="登录">
+                <em class="icon18 login-icon">&nbsp;</em>
+                <span class="vam ml5">登录</span>
+              </a>
+              |
+              <a href="/sign_up" title="注册">
+                <span class="vam ml5">注册</span>
+              </a>
+            </li>
+            <li class="mr10 undis" id="is-login-one">
+              <a href="#" title="消息" id="headerMsgCountId">
+                <em class="icon18 news-icon">&nbsp;</em>
+              </a>
+              <q class="red-point" style="display: none">&nbsp;</q>
+            </li>
+            <li class="h-r-user undis" id="is-login-two">
+              <a href="#" title>
+                <img
+                  src="@/assets/img/avatar-boy.gif"
+                  width="30"
+                  height="30"
+                  class="vam picImg"
+                  alt
+                >
+                <span class="vam disIb" id="userName"></span>
+              </a>
+              <a href="javascript:void(0)" title="退出" onclick="exit();" class="ml5">退出</a>
+            </li>
+            <!-- /未登录显示第1 li；登录后显示第2，3 li -->
+          </ul>
+          <aside class="h-r-search">
+            <form action="#" method="post">
+              <label class="h-r-s-box">
+                <input type="text" placeholder="输入你想学的课程" name="queryCourse.courseName" value>
+                <button type="submit" class="s-btn">
+                  <em class="icon18">&nbsp;</em>
+                </button>
+              </label>
+            </form>
+          </aside>
+        </div>
+        <aside class="mw-nav-btn">
+          <div class="mw-nav-icon"></div>
+        </aside>
+        <div class="clear"></div>
+      </section>
+    </header>
+    <!-- /公共头引入 -->
+
+    <nuxt/>
+
+    <!-- 公共底引入 -->
+    <footer id="footer">
+      <section class="container">
+        <div class>
+          <h4 class="hLh30">
+            <span class="fsize18 f-fM c-999">友情链接</span>
+          </h4>
+          <ul class="of flink-list">
+            <li>
+              <a href="https://github.com/andochiwa" title="github" target="_blank">我的github</a>
+            </li>
+          </ul>
+          <div class="clear"></div>
+        </div>
+        <div class="b-foot">
+          <section class="fl col-7">
+            <section class="mr20">
+              <section class="b-f-link">
+                <a href="#" title="关于我们" target="_blank">关于我们</a>|
+                <a href="#" title="联系我们" target="_blank">联系我们</a>|
+                <a href="#" title="帮助中心" target="_blank">帮助中心</a>|
+                <a href="#" title="资源下载" target="_blank">资源下载</a>|
+                <span>服务热线：010-56253825(北京) 0755-85293825(深圳)</span>
+                <span>Email：a1066079469@gmail.com</span>
+              </section>
+              <section class="b-f-link mt10">
+                <span>在线教育网站</span>
+              </section>
+            </section>
+          </section>
+          <aside class="fl col-3 tac mt15">
+            <section class="gf-tx">
+              <span>
+                <img src="@/assets/img/wx-icon.png" alt>
+              </span>
+            </section>
+            <section class="gf-tx">
+              <span>
+                <img src="@/assets/img/wb-icon.png" alt>
+              </span>
+            </section>
+          </aside>
+          <div class="clear"></div>
+        </div>
+      </section>
+    </footer>
+    <!-- /公共底引入 -->
   </div>
 </template>
+<script>
+import "@/assets/css/reset.css";
+import "@/assets/css/theme.css";
+import "@/assets/css/global.css";
+import "@/assets/css/web.css";
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+export default {};
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'education-frontend-web',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: '在线教育平台',
+    // htmlAttrs: {
+    //   lang: 'en'
+    // },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -17,12 +17,17 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    'swiper/swiper-bundle.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/element-ui'
+    '@/plugins/element-ui',
+    {
+      src: '@/plugins/awesome-swiper.js',
+      ssr: false
+    }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
